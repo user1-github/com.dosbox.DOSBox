49eed240284482cd828db10c21884cb243f2b682
user1-github <56021366+user1-github@users.noreply.github.com>
2026-03-15 20:06:19 +0200

    Update startup script to make DOSBox create the config file under /config/dosbox

--- run-dosbox.sh
+++ run-dosbox.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-exec dosbox -conf "$XDG_CONFIG_HOME"/dosbox/dosbox.conf "$@"
+
+CONF_PATH="$XDG_CONFIG_HOME/dosbox/dosbox.conf"
+
+mkdir -p "$(dirname "$CONF_PATH")"
+
+if [ ! -f "$CONF_PATH" ]; then
+    echo "Config not found. Generating default at $CONF_PATH..."
+    dosbox -eraseconf -writeconf "$CONF_PATH" -exit
+fi
+
+exec /app/bin/dosbox -conf "$CONF_PATH" "$@"
