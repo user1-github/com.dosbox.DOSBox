9db2d12c106cb25a1f3ee8b0d5a2db4d235eb13a
user1-github <56021366+user1-github@users.noreply.github.com>
2026-03-15 23:09:22 +0200

    More startup script fixes

--- run-dosbox.sh
+++ run-dosbox.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-CONF="$HOME/.config/dosbox/dosbox.conf"
 
-[ -f "$CONF" ] || dosbox -writeconf "$CONF"
+CONF_PATH="$XDG_CONFIG_HOME/dosbox.conf"
 
-exec dosbox -conf "$CONF" "$@"
+if [ ! -f "$CONF_PATH" ]; then
+    echo "Config not found. Generating default at $CONF_PATH..."
+    dosbox -c "CONFIG -writeconf $CONF_PATH" -exit
+fi
+
+exec dosbox -conf "$CONF_PATH" "$@"
